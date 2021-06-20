2d260fbc2a8df69652b57439a41c7094e8f73ab6
morzarein <morteza.zarein@yahoo.com>
2021-06-20 19:44:19 +0430

    test

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 import requests
-get_token = requests.post('https://ipam.sic.net/api/snsg/user/',
-                          auth=('py_api', 'TZ@T&Nq6Ar73'), verify=False).json()
+
 print(get_token)
 a = 1
 b = 2
 print(a+b)
 c=a+b
+a=1
+
